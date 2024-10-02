68e87454385e81104c004995a3f32610c53144d4
akashgupta757 <gakash189189@gmail.com>
2024-10-02 01:46:09 -0400

    Defined getters and setters for TransactionDTO.java.java

---
+++ src/main/java/com/example/banking_transactions/dto/TransactionDTO.java
@@ -0,0 +1,31 @@
+package com.example.banking_transactions.dto;
+
+public class TransactionDTO {
+
+    private Long toAccountId;
+
+    private double amount;
+
+    public TransactionDTO() {}
+
+    public TransactionDTO(Long toAccountId, double amount){
+        this.toAccountId = toAccountId;
+        this.amount = amount;
+    }
+
+    public Long getToAccountId(){
+        return toAccountId;
+    }
+
+    public void setToAccountId(Long toAccountId){
+        this.toAccountId = toAccountId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+}
