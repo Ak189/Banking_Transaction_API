2db6f9344bc3d1421a385a514c64a4b78ff0d743
akashgupta757 <gakash189189@gmail.com>
2024-10-02 01:33:59 -0400

    Defined getters and setters for AccountDTO.java

---
+++ src/main/java/com/example/banking_transactions/dto/AccountDTO.java
@@ -0,0 +1,31 @@
+package com.example.banking_transactions.dto;
+
+
+
+public class AccountDTO {
+    private String name;
+    private double initialBalance;
+
+    public AccountDTO() {}
+
+    public AccountDTO(String name, double initialBalance){
+        this.name = name;
+        this.initialBalance = initialBalance;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getInitialBalance() {
+        return initialBalance;
+    }
+
+    public void setInitialBalance(double initialBalance){
+        this.initialBalance = initialBalance;
+    }
+}
