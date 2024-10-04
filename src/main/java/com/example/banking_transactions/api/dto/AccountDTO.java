604899cf78b1bad7032f166d8b8fe994e18fc100
akashgupta757 <gakash189189@gmail.com>
2024-10-04 01:51:37 -0400

    Code Restructuring and documentation.

--- src/main/java/com/example/banking_transactions/api/dto/AccountDTO.java
+++ src/main/java/com/example/banking_transactions/api/dto/AccountDTO.java
@@ -3,23 +3,27 @@ package com.example.banking_transactions.api.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-
-
+//Data transfer object class for account creation
 public class AccountDTO {
+    //Name can't be null, using a validation method.
     @NotNull(message = "Name cannot be null")
     private String name;
+    //Initial Balance can't be negative.
     @Min(value = 0, message = "Initial balance must be positive")
     private double initialBalance;
 
+    //Constructor class to initiate name and balane.
     public AccountDTO(String name, double initialBalance){
         this.name = name;
         this.initialBalance = initialBalance;
     }
 
+    //Getter method for name.
     public String getName(){
         return name;
     }
 
+    //Getter method for initial balance
     public double getInitialBalance() {
         return initialBalance;
     }
--- src/main/java/com/example/banking_transactions/api/dto/TransactionDTO.java
+++ src/main/java/com/example/banking_transactions/api/dto/TransactionDTO.java
@@ -1,20 +1,20 @@
 package com.example.banking_transactions.api.dto;
-
+//Data transfer object class to handle Transaction details
 public class TransactionDTO {
-
+    //Field for receiver's account
     private Long toAccountId;
-
+    //Field for the amount to be transferred
     private double amount;
-
+    //Constructor class
     public TransactionDTO(Long toAccountId, double amount){
         this.toAccountId = toAccountId;
         this.amount = amount;
     }
-
+    //Getter method to retrieve the ID of the receiver's account.
     public Long getToAccountId(){
         return toAccountId;
     }
-
+    //Getter method to retrieve the amount to be transferred.
     public double getAmount() {
         return amount;
     }
