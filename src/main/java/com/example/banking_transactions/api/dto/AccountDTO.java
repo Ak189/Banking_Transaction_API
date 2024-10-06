51aeaaed013b457a2420f983388cf5000b004937
akashgupta757 <gakash189189@gmail.com>
2024-10-06 14:51:16 -0400

    Final documentation for AccountDTO.java.

--- src/main/java/com/example/banking_transactions/api/dto/AccountDTO.java
+++ src/main/java/com/example/banking_transactions/api/dto/AccountDTO.java
@@ -3,39 +3,44 @@ package com.example.banking_transactions.api.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+/**
+ * Data transfer object class, Used to transfer data between client and server
+ */
+
 public class AccountDTO {
+    //Profile first name, can't be null.
     @NotNull(message = "First name cannot be null")
     private String firstName;
-
+    //Profile middle name, (optional)
     private String middleName;
-
+    //Profile last name, can't be null.
     @NotNull(message = "Last name cannot be null")
     private String lastName;
-
+    //Profile address line 1, can't be null.
     @NotNull(message = "Address line 1 cannot be null")
     private String addressLine1;
-
+    //Profile address line 2, (optional).
     private String addressLine2;
-
+    //Profile city, required.
     @NotNull(message = "City cannot be null")
     private String city;
-
+    //Profile's State, can't be null.
     @NotNull(message = "State cannot be null")
     private String state;
-
+    //Profile's postal code, required.
     @NotNull(message = "Postal code cannot be null")
     @Size(min = 5, max = 10, message = "Postal code must be between 5 and 10 characters")
     private String postalCode;
-
+    //Profile's driving license number, required and can't be duplicate.
     @NotNull(message = "Driving license cannot be null")
     private String drivingLicense;
-
+    //Profile's SIN number, required and can't be duplicate.
     @NotNull(message = "SIN number cannot be null")
     @Size(min = 9, max = 9, message = "SIN number must be 9 digits")
     private String sinNumber;
-
+    //Initial balance
     private double initialBalance;
-
+    //Constructor class to create a new AccountDTO instance with all of the above as parameters.
     public AccountDTO(String firstName, String middleName, String lastName, String addressLine1, String addressLine2,
                       String city, String state, String postalCode, String drivingLicense, String sinNumber, double initialBalance) {
         this.firstName = firstName;
@@ -50,6 +55,7 @@ public class AccountDTO {
         this.sinNumber = sinNumber;
         this.initialBalance = initialBalance;
     }
+    //Getters and Setters method for all of the parameters.
 
     public String getFirstName() {
         return firstName;
@@ -134,7 +140,6 @@ public class AccountDTO {
     public double getInitialBalance() {
         return initialBalance;
     }
-
     public void setInitialBalance(double initialBalance) {
         this.initialBalance = initialBalance;
     }
